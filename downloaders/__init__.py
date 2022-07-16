762beeaa3691570da1a7fe38dfc82cb47a5ffa33
Niki Bizjak <drobilc@gmail.com>
2022-07-16 11:28:24 +0200

    Add wattpad story downloader

--- downloaders/__init__.py
+++ downloaders/__init__.py
@@ -1,6 +1,8 @@
 from .exceptions import *
 from .fanfiction import FanfictionDownloader
+from .wattpad import WattpadDownloader
 
 DOWNLOADERS = {
     'fanfiction': FanfictionDownloader,
+    'wattpad': WattpadDownloader,
 }
