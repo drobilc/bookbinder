207ee40a00dd8657b795b84e5c42e06ba371695c
drobilc <drobilc@gmail.com>
2023-09-09 10:40:47 +0200

    Remove the not working Wattpad downloader

--- downloaders/__init__.py
+++ downloaders/__init__.py
@@ -1,10 +1,8 @@
 from .exceptions import *
 from .fanfiction import FanfictionDownloader
-from .wattpad import WattpadDownloader
 from .json import JSONDownloader
 
 DOWNLOADERS = {
     'fanfiction': FanfictionDownloader,
-    'wattpad': WattpadDownloader,
     'json': JSONDownloader,
 }
