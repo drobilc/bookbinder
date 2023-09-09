a54a3d07a02f74726767aeac92c10b93dbbc58e8
drobilc <drobilc@gmail.com>
2023-09-09 10:08:16 +0200

    Enforce some structure in the `Ebook`object

--- downloaders/ebook.py
+++ downloaders/ebook.py
@@ -1,5 +1,15 @@
 class Ebook(object):
     
-    def __init__(self, metadata, data):
-        self.metadata = metadata
-        self.data = data
+    def __init__(self, title=None, id=None, language='en', authors=[], description=None, chapters=[]):
+        self.title = title
+        self.id = id
+        self.language = language
+        self.authors = authors
+        self.description = description
+        self.chapters = chapters
+
+class Chapter(object):
+
+    def __init__(self, title, html):
+        self.title = title
+        self.html = html
